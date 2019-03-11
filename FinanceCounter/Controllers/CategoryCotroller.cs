02077bce3c8842d1b27d1153e5d933aad227f0f7
Guest User <Guest@epicodus-26.local>
2019-03-11 14:38:24 -0700

    add category controller

--- FinanceCounter/Controllers/CategoryCotroller.cs
+++ FinanceCounter/Controllers/CategoryCotroller.cs
@@ -1,1 +1,76 @@
 
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using FinanceCounter.Models;
+
+namespace FinanceCounter.Controllers
+{
+  public class CategoryController : Controller
+  {
+    [HttpGet("/categories")]
+    public ActionResult Index()
+    {
+      List<Category> allCategories = Category.GetAll();
+      return View(allCategories);
+    }
+
+    [HttpGet("/categories/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/categories")]
+    public ActionResult Create(string categoryName)
+    {
+      Category newCategory = new Category(categoryName);
+      newCategory.Save();
+      List<Category> allCategories = Category.GetAll();
+      return View("Index", allCategories);
+    }
+
+    [HttpGet("/categories/{id}")]
+    public ActionResult Show(int id)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(id);
+      List<Item> categoryItems = selectedCategory.GetItems();
+      List<Item> allItems = Item.GetAll();
+      List<Item> categoryItems = selectedCategory.GetItems();
+      model.Add("category", selectedCategory);
+      model.Add("categoryItems", categoryItems);
+      model.Add("items", categoryItems);
+      model.Add("allItems", allItems);
+      return View(model);
+    }
+
+    [HttpPost("/categories/{categoryId}/items")]
+    public ActionResult Create(int categoryId, string itemName, int itemPhone)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Category selectedCategory = Category.Find(categoryId);
+      Item newItem = new Item(categoryId, itemName, itemPhone);
+      newItem.Save();
+      List<Item> categoryItems = selectedCategory.GetItems();
+      List<Item> allItems = Item.GetAll();
+      List<Item> categoryItems = selectedCategory.GetItems();
+      model.Add("items", categoryItems);
+      model.Add("categoryItems", categoryItems);
+      model.Add("category", selectedCategory);
+      model.Add("allItems", allItems);
+      return View("Show", model);
+    }
+
+    [HttpPost("/categories/{categoryId}/items/new")]
+    public ActionResult AddItem(int categoryId, int itemId)
+    {
+      Category category = Category.Find(categoryId);
+      Item item = Item.Find(itemId);
+      category.AddItem(item);
+      return RedirectToAction("Show",  new { id = categoryId });
+    }
+
+
+  }
+}
