6c3943e1f54fd3835c61f4605b38c7a3b6de9e7c
Guest User <Guest@epicodus-26.local>
2019-03-11 13:38:09 -0700

    add backend

--- FinanceCounter/Models/Categories.cs
+++ FinanceCounter/Models/Categories.cs
@@ -1,5 +1,5 @@
 using System.Collections.Generic;
-using MySql.Data.MySqlClient;
+using MySql.Data.MySqlItem;
 
 namespace HairSalon.Models
 {
@@ -9,7 +9,7 @@ namespace HairSalon.Models
     private string _name;
     private double _total;
 
-    public Stylist(string name, double total; int id = 0)
+    public Stylist(string name, double total = 0, int id = 0)
     {
       _name = name;
       _id = id;
@@ -24,21 +24,128 @@ namespace HairSalon.Models
     public void SetName(string newName){_name = newName;}
     public void SetTotal(double newTotal){_total = newTotal;}
 
+    public static List<Category> GetAll()
+    {
+      List<Category> allCategories = new List<Category> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int categoryId = rdr.GetInt32(0);
+        string categoryName = rdr.GetString(1);
+        double categoryTotal = rdr.GetDouble(2);
+        Category newCategory = new Category(categoryName, categoryTotal, categoryId);
+        allCategories.Add(newCategory);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allCategories;
+    }
+
+    public static Category Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM categories WHERE id = (@searchId);";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int categoryId = 0;
+      string categoryName = "";
+      double categoryTotal = 0;
+      while(rdr.Read())
+      {
+        categoryId = rdr.GetInt32(0);
+        categoryName = rdr.GetString(1);
+        categoryTotal = rdr.GetDouble(2);
+      }
+      Category newCategory = new Category(categoryName, categoryTotal, categoryId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newCategory;
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO categories (name) VALUES (@name);";
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@name";
+      name.Value = this._name;
+      cmd.Parameters.Add(name);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public List<Item> GetItems()
+    {
+      List<Item> allStylistItems = new List<Item>{};
+      MySqlConnection conn = DB.Connection();
+     conn.Open();
+     var cmd = conn.CreateCommand() as MySqlCommand;
+     cmd.CommandText = "SELECT * FROM items WHERE stylist_id = @stylist_id;";
+     MySqlParameter stylistId = new MySqlParameter();
+      stylistId.ParameterName = "@stylist_id";
+      stylistId.Value = this._id;
+      cmd.Parameters.Add(stylistId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        int categpryId = rdr.GetInt32(1);
+        string itemName = rdr.GetString(2);
+        double itemPrice = rdr.GetDouble(3);
+        Item newItem= new Item(categpryId, itemName, itemPrice, itemId);
+        allStylistItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allStylistItems;
+    }
 
 
-    // public override bool Equals(System.Object otherStylist)
+
+
+
+
+
+
+// // ==========================TESTS =========================================
+    // `public override bool Equals(System.Object otherCategory)
     // {
-    //     if (!(otherStylist is Stylist))
-    //     {
-    //       return false;
-    //     }
-    //     else
-    //     {
-    //       Stylist newStylist = (Stylist) otherStylist;
-    //       bool idEquality = this.GetId().Equals(newStylist.GetId());
-    //       bool nameEquality = this.GetName().Equals(newStylist.GetName());
-    //       return (idEquality && nameEquality);
-    //     }
+    //   if (!(otherCategory is Category))
+    //   {
+    //     return false;
+    //   }
+    //   else
+    //   {
+    //     Category newCategory = (Category) otherCategory;
+    //     bool idEquality = this.GetId().Equals(newCategory.GetId());
+    //     bool nameEquality = this.GetName().Equals(newCategory.GetName());
+    //     return (idEquality && nameEquality);
+    //   }
     // }
     // public override int GetHashCode()
     // {
@@ -50,7 +157,7 @@ namespace HairSalon.Models
     //   MySqlConnection conn = DB.Connection();
     //   conn.Open();
     //   var cmd = conn.CreateCommand() as MySqlCommand;
-    //   cmd.CommandText = @"DELETE  FROM stylists;";
+    //   cmd.CommandText = @"DELETE FROM categories;";
     //   cmd.ExecuteNonQuery();
     //   conn.Close();
     //   if (conn != null)
---
+++ FinanceCounter/Models/Item.cs
@@ -0,0 +1,161 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace FinanceCounter.Models
+{
+  public class Item
+  {
+    private string _name;
+    private int _id;
+    private int _categoryId;
+    private double _price;
+
+    public Item (string name, double price, int categoryId, int id = 0)
+    {
+      _name = name;
+      _categoryId = categoryId;
+      _id = id;
+      _price = price;
+    }
+
+    public string GetName(){return _name;}
+    public double GetPrice(){return _price;}
+    public int GetId(){ return _id;}
+    public int GetCategoryId(){ return _categoryId;}
+
+    public void SetName(string newName){ _name = newName;}
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemName = rdr.GetString(1);
+        double itemPrice = rdr.GetDouble(2);
+        int itemCategoryId = rdr.GetInt32(3);
+        Item newItem = new Item(itemName, itemPrice, itemCategoryId, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allItems;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+       conn.Dispose();
+      }
+    }
+
+    public static Item Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items WHERE id = (@searchId);";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int itemId = 0;
+      string itemName = "";
+      int itemCategoryId = 0;
+      double itemPrice = 0;
+      while(rdr.Read())
+      {
+        itemId = rdr.GetInt32(0);
+        itemName = rdr.GetString(1);
+        itemPrice = rdr.GetDouble(2);
+        itemCategoryId = rdr.GetInt32(3);
+      }
+      Item newItem = new Item(itemName, itemPrice, itemCategoryId, itemId);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newItem;
+    }
+
+    public override bool Equals(System.Object otherItem)
+    {
+      if (!(otherItem is Item))
+      {
+        return false;
+      }
+      else
+      {
+         Item newItem = (Item) otherItem;
+         bool idEquality = this.GetId() == newItem.GetId();
+         bool nameEquality = this.GetName() == newItem.GetName();
+         bool categoryEquality = this.GetCategoryId() == newItem.GetCategoryId();
+         return (idEquality && nameEquality && categoryEquality);
+       }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO items (name, category_id) VALUES (@name, @category_id);";
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@name";
+      name.Value = this._name;
+      cmd.Parameters.Add(name);
+      MySqlParameter categoryId = new MySqlParameter();
+      categoryId.ParameterName = "@category_id";
+      categoryId.Value = this._categoryId;
+      cmd.Parameters.Add(categoryId);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Edit(string newName)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"UPDATE items SET name = @newName WHERE id = @searchId;";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = _id;
+      cmd.Parameters.Add(searchId);
+      MySqlParameter name = new MySqlParameter();
+      name.ParameterName = "@newName";
+      name.Value = newName;
+      cmd.Parameters.Add(name);
+      cmd.ExecuteNonQuery();
+      _name = newName;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+  }
+}
