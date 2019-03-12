48a868eb0a4f1c2c1aa0948d5c29df462b0510f5
Guest User <Guest@epicodus-26.local>
2019-03-12 15:38:05 -0700

    add expense controllers

--- FinanceCounter/Controllers/ExpenseCategoryController.cs
+++ FinanceCounter/Controllers/ExpenseCategoryController.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Generic;
 using System;
 using Microsoft.AspNetCore.Mvc;
@@ -9,13 +8,13 @@ namespace FinanceCounter.Controllers
   public class ExpenseCategoryController : Controller
   {
 
-    [HttpGet("/expense/categories/new")]
+    [HttpGet("/expense/new")]
     public ActionResult New()
     {
       return View();
     }
 
-    [HttpGet("/expense/categories/{id}")]
+    [HttpGet("/expense/{id}")]
     public ActionResult Show(int id)
     {
       Dictionary<string, object> model = new Dictionary<string, object>();
@@ -26,7 +25,7 @@ namespace FinanceCounter.Controllers
       return View(model);
     }
 
-    [HttpPost("accounts/{accountId}/expense/categories")]
+    [HttpPost("accounts/{accountId}/expense")]
     public ActionResult Create(int accountId, string name)
     {
       ExpenseCategory newExpenseCategory = new ExpenseCategory(accountId, name);
@@ -34,18 +33,35 @@ namespace FinanceCounter.Controllers
       return RedirectToAction("Show", "Account", new {id = accountId});
     }
 
+    [HttpPost("/expense/{id}/delete")]
+    public ActionResult Delete(int id)
+    {
+      ExpenseCategory expenseCategory = ExpenseCategory.Find(id);
+      expenseCategory.Delete();
+      return View();
+    }
 
+    [HttpPost("/expense/delete")]
+    public ActionResult DeleteAll(int id)
+    {
+      ExpenseCategory.Delete();
+      return View();
+    }
 
-    // [HttpPost("/expense/categories/{expenseCategoryId}/items/new")]
-    // public ActionResult AddExpenseItem(int expenseCategoryId, int exitemId)
-    // {
-    //   ExpenseCategory expenseCategory = ExpenseCategory.Find(expenseCategoryId);
-    //   ExpenseItem exitem = ExpenseItem.Find(exitemId);
-    //   expenseCategory.AddExpenseItem(exitem);
-    //   return RedirectToAction("Show",  new { id = expenseCategoryId });
-    // }
+    [HttpPost("/expense/{id}/update")]
+    public ActionResult Update(int id, string newName)
+    {
+      ExpenseCategory expenseCategory = ExpenseCategory.Find(id);
+      expenseCategory.Edit(newName);
+      return RedirectToAction("Show", id);
+    }
 
+    [HttpPost("/expense/{id}/edit")]
+    public ActionResult Edit(int id)
+    {
+      ExpenseCategory expenseCategory = ExpenseCategory.Find(id);
+      return View(expenseCategory);
+    }
 
   }
 }
-
